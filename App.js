482f827bab313412ca822c86cd623e76527ce900
Romain TEXIER <rtexier@solocal.com>
2022-11-08 18:52:18 +0100

    @add: icons

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
     return (
@@ -16,9 +17,19 @@ function HomeScreen() {
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-            </Stack.Navigator>
+            <Tab.Navigator screenOptions={({route}) => ({
+                tabBarIcon: ({_, color, size}) => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                        iconName = 'home';
+                    } else if (route.name === 'Settings') {
+                        iconName = 'settings';
+                    }
+                    return <Icon name={iconName} size={size} color={color} />;
+                }
+            })}>
+                <Tab.Screen name="Home" component={HomeScreen} />
+            </Tab.Navigator>
         </NavigationContainer>
     )
 }
